9c9317c842980800645e20ee3087e3fa9e4de47a
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 16:10:57 +0100

    Create list

--- zad9_3_1.py
+++ zad9_3_1.py
@@ -2,9 +2,11 @@ import random
 
 f = open('file2.txt', 'w')
 
+char = [' ', ' ', ' ', ' ', '*']
+
 for item in range(50):
     for item in range(50):
-        random_char = random.choice([' ', '*'])
+        random_char = random.choice(char)
         f.write(random_char)
     f.write("\n")
 f.close()
