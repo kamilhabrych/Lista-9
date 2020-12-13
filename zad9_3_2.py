c259f378189c05c427f6255088f7486847bea71a
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 16:31:41 +0100

    Update files

--- zad9_3_2.py
+++ zad9_3_2.py
@@ -2,10 +2,11 @@ import random
 
 f = open('file3.txt', 'w')
 
-char = [' ', '*']
+char = [' ',' ',' ',' ', '*']
 
 for item in range(50):
-    for item in range(50):
-        
+    for item in range(5):
+        random_char = random.choice(char)        
+        f.write(random_char)
     f.write("\n")
 f.close()
