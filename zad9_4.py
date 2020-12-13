c888227ce74551706810be064365486d9121696e
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 16:52:50 +0100

    Generate prime numbers to 100

---
+++ zad9_4.py
@@ -0,0 +1,11 @@
+f = open('pierwsze.txt', 'w')
+
+for item in range(2,100+1):
+    for number in range(2,item):
+        if item % number == 0:
+            break
+    else:
+        f.write(str(item))
+        f.write(" ")
+
+f.close()
