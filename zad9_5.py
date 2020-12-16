eec196f2f68d05e8dab53bce1f5f95624eeac5c7
Kamil Habrych <habrychkamil@gmail.com>
2020-12-16 12:43:11 +0100

    Update task

--- zad9_5.py
+++ zad9_5.py
@@ -2,22 +2,23 @@ import math
 def roundUp(x):
     return int(math.ceil(x/100))*100
 
-with open('pierwsze.txt', 'r') as fo:
-    for s in fo: 
+with open('pierwsze.txt', 'r') as f1:
+    for s in f1: 
         pass
     s = s.replace(' ', 'x')
     l = s.split('x')
     l.pop()
     last_digit = l.pop()
-    fo.close()
+    f1.close()
 
-with open('pierwsze.txt', 'a') as fa:
+with open('pierwsze.txt', 'a') as f2:
     n = roundUp(int(last_digit))
     m = n + 101
-    fa.write("\n")
-    
+    f2.write("\n")
     for i in range(n,m):
         for j in range(2,i):
-            if (i % j) == 0: break
-        else: fa.write(str(i) + " ")
-    fa.close()
+            if (i % j) == 0: 
+                break
+        else: 
+            f2.write(str(i) + " ")
+    f2.close()
