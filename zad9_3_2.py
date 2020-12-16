5a7d80eff58fdb6cac95fe5643479a284d2294d2
Kamil Habrych <habrychkamil@gmail.com>
2020-12-16 11:40:20 +0100

    Task complete

--- zad9_3_2.py
+++ zad9_3_2.py
@@ -5,7 +5,7 @@ f = open('file3.txt', 'w')
 char = [' ',' ',' ',' ', '*']
 
 for item in range(50):
-    for item in range(5):
+    for item in range(50):
         random_char = random.choice(char)        
         f.write(random_char)
     f.write("\n")
