86a081f51e008c6b2b9188a2905c3c5808c481bb
Kamil Habrych <habrychkamil@gmail.com>
2020-12-16 11:18:20 +0100

    Task complete

--- zad9_2.py
+++ zad9_2.py
@@ -2,4 +2,18 @@ f = open("liczby.txt", "r")
 
 l = []
 
+space = ""
+
+while True:
+    s = f.read(1)
+    if s == "":
+        break
+    if s.isnumeric():
+        space += str(s)
+    elif s.isspace():
+        l.append(int(space))
+        space = ""
+
+print(l)
+
 f.close()
