fd1cab6b7e43619431df4f37b19fd71cc1465de9
Kamil Habrych <habrychkamil@gmail.com>
2020-12-16 12:31:40 +0100

    Update task

--- zad9_5.py
+++ zad9_5.py
@@ -1,6 +1,23 @@
-f = open("pierwsze.txt", "r")
-read = f.read()
-list_read = []
-for item in read:
-    print(item)
-f.close()
+import math
+def roundUp(x):
+    return int(math.ceil(x/100))*100
+
+with open('pierwsze.txt', 'r') as fo:
+    for s in fo: 
+        pass
+    s = s.replace(' ', 'x')
+    l = s.split('x')
+    l.pop()
+    last_digit = l.pop()
+    fo.close()
+
+with open('pierwsze.txt', 'a') as fa:
+    n = roundUp(int(last_digit))
+    m = n + 101
+    fa.write("\n")
+    
+    for i in range(n,m):
+        for j in range(2,i):
+            if (i % j) == 0: break
+        else: fa.write(str(i) + " ")
+    fa.close()
