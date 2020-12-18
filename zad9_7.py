96f79a48d14730d25c2e9ed85d8e798b1dac9299
Kamil Habrych <habrychkamil@gmail.com>
2020-12-18 19:25:07 +0100

    Task complete

--- zad9_7.py
+++ zad9_7.py
@@ -5,32 +5,37 @@ class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-def randomizePoints(point):
+
+def randomPoints(point):
     point.x = random.randint(1,50)
     point.y = random.randint(1,50)
-def printPattern(radius,point):
+
+def printCircle(radius,point):
     for i in range((2 * radius)+1):
         for j in range((2 * radius)+1):
-            dist = math.sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius))
-            if (dist > radius - 0.5 and dist < radius + 0.5):
-                listOfPoints.append(Point(point.y+radius-j,point.x+radius-i))
-notFound = True
-listOfPoints = []
-p = Point(0,0)
-randomizePoints(p)
+            x = math.sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius))
+            if (x > radius - 0.5 and x < radius + 0.5):
+                Points.append(Point(point.y+radius-j,point.x+radius-i))
 
-while 1:
+notFound = True
+Points = []
+p = Point(0,0)
+randomPoints(p)
+
+while True:
     radius = random.randint(1,50)
-    if p.x+radius <= 50 and p.x - radius >= 1 and p.y+radius <= 50 and p.y-radius >= 1:
+
+    if p.x + radius <= 50 and p.x - radius >= 1 and p.y + radius <= 50 and p.y - radius >= 1:
         break
-printPattern(radius,p)
+    
+printCircle(radius,p)
 
 with open('okrag.txt','w') as f1:
-    for row in range(1,51):
-        for column in range(1,51):
+    for row in range(1,50+1):
+        for column in range(1,50+1):
             notFound = True
-            for obj in listOfPoints:
-                if (column == obj.x and row == obj.y):
+            for i in Points:
+                if (column == i.x and row == i.y):
                     f1.write("*")
                     notFound = False
             if notFound == True: 
