fed82aca10cc0dd5b76f1f85e04a2c18c4e17278
Kamil Habrych <habrychkamil@gmail.com>
2020-12-18 19:15:42 +0100

    Update tasks

--- zad9_7.py
+++ zad9_7.py
@@ -1,39 +1,39 @@
-import random, math
+import random
+import math
+
 class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
 def randomizePoints(point):
-point.x = random.randint(1,50)
+    point.x = random.randint(1,50)
     point.y = random.randint(1,50)
 def printPattern(radius,point):
     for i in range((2 * radius)+1):
         for j in range((2 * radius)+1):
-            dist = math.sqrt((i - radius) * (i - radius) +
-                  (j - radius) * (j - radius))
+            dist = math.sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius))
             if (dist > radius - 0.5 and dist < radius + 0.5):
-                listOfPoints.append(Point(point.y+radius-
-j,point.x+radius-i))
+                listOfPoints.append(Point(point.y+radius-j,point.x+radius-i))
 notFound = True
 listOfPoints = []
 p = Point(0,0)
 randomizePoints(p)
+
 while 1:
     radius = random.randint(1,50)
-    if p.x+radius <= 50 and p.x-
-radius >= 1 and p.y+radius <= 50 and p.y-radius >= 1:
-break
+    if p.x+radius <= 50 and p.x - radius >= 1 and p.y+radius <= 50 and p.y-radius >= 1:
+        break
 printPattern(radius,p)
-with open('okrag.txt','w') as fo:
-for row in range(1,51):
+
+with open('okrag.txt','w') as f1:
+    for row in range(1,51):
         for column in range(1,51):
             notFound = True
             for obj in listOfPoints:
                 if (column == obj.x and row == obj.y):
-                    fo.write("*")
+                    f1.write("*")
                     notFound = False
-            if notFound == True: fo.write(" ")
-        fo.write("\n")
-    fo.write("Okrag o punkcie centrum p=({0},{1}) i promieniu
-{2}".format(p.x,p.y,radius))
-    fo.close()
+            if notFound == True: 
+                f1.write(" ")
+        f1.write("\n")
+    f1.close()
